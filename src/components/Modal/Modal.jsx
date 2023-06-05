96c796fce0085e76ebf983fe1f4a9b70cb6e9552
Alexander Ratushnyi <olexandr.ratushnyi@gmail.com>
2023-06-05 17:30:46 +0300

    close modal on escape

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,6 +2,20 @@ import { Component } from 'react';
 import css from './Modal.module.css';
 
 export default class Modal extends Component {
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown = evt => {
+    if (evt.code === 'Escape') {
+      this.props.onClose();
+    }
+  };
+
   onOverlayClick = evt => {
     if (evt.currentTarget === evt.target) {
       this.props.onClose();
